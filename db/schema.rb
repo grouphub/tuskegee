43e916b3f5ef22a5db711524ac2c6a98b11fdb77
allenbyerly <allen@tritl.com>
2014-09-07 13:44:04 -0700

    postgres change for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140906204900) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "applicants", force: true do |t|
     t.string   "first_name"
     t.string   "middle_name"
